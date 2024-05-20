6bc9da385feb2db5784dc81a57fbfa146c9aa075
1juanherrera <juansebas751@gmail.com>
2024-05-20 16:52:06 -0500

    feat(Sidebar): added styles

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,14 @@
+
+
+const SideBar = () => {
+    return (
+        <div className="bg-gray-200 p-4 rounded-lg">
+            <h2 className="font-bold text-xl mb-4">Sidebar</h2>
+            <p>Este es un ejemplo de una barra lateral.</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id est commodi ullam atque voluptatum laudantium officiis eos soluta vitae, quas tenetur delectus praesentium non asperiores, quibusdam quod qui et veniam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio iure sunt molestias quasi, temporibus autem voluptatum corrupti eaque vel repellendus veniam totam assumenda ea nisi, atque quam ad quibusdam error! Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio inventore harum fugit corporis culpa labore voluptatum, nostrum laboriosam unde mollitia fugiat repellat quisquam odio. At libero aliquid dolore ratione iste?</p>
+        </div>
+    )
+}
+
+
+export default SideBar
